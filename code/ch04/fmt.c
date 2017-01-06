2fe19a91c8e75ec26e3a66985c79a1933108136e
embpgp <522943492@qq.com>
2017-01-06 22:24:53 +0800

    test problem

--- code/ch04/fmt.c
+++ code/ch04/fmt.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+/*I have changed the code,so let it work*/
+
+/*In order to implement arbitrary address write I need some local vars and strcpy them.*/
+/*But I don't need overflow it*/
 
 int main( int argc, char *argv[] )
 {
-        printf( argv[1] );
-
+	char evil[128] = {0};
+	strcpy(evil, argv[1]);
+	printf( argv[1]);
+	return 0;
 }
